5b115abfee95034990bde98593f32e80a3bf6356
Maiky33 <maicolbarrerag@gmail.com>
2024-07-30 17:26:55 -0500

    inlaze Movies

--- Server/src/users/users.module.ts
+++ Server/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule} from '@nestjs/mongoose';
 import { UserSchema, User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports:[MongooseModule.forFeature([{name:User.name, schema:UserSchema}])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtService],
 })
 export class UsersModule {}
