05fdb61f1deababa89f75d8ed55713484b37f0a7
Maiky33 <maicolbarrerag@gmail.com>
2024-07-30 16:44:11 -0500

    inlaze Movies

--- Client/src/Components/ComponentNotfoundPage.jsx
+++
@@ -1,7 +0,0 @@
-import '../css/ComponentNotfoundPage.css'
-
-const ComponentNotfoundPage = () => {
-  //pasamos item por parametros para usarlo en el map
-  return <div className="containerNotfound">404 <br/>Not Found Page</div>;
-};
-export default ComponentNotfoundPage;
--- README.md
+++
@@ -1,1 +0,0 @@
-"# inlaze_Movies" 
