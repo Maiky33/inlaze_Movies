f27a3b0d76c4c303368fd234f0898b210f99eace
Maiky33 <maicolbarrerag@gmail.com>
2024-08-30 10:48:16 -0500

    vercel test

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import cors from 'cors';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'https://inlaze-movies-client.vercel.app',
+  app.use(cors({
+    origin: "https://inlaze-movies-client.vercel.app", 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  });
+  })); 
 
   app.use(cookieParser());
   
